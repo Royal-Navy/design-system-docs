a3e7887154c67d47d693e2295ee1ebbe1afc7918
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-02-15 09:09:50 +0000

    build(NextJS): Setup boilerplate application

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,31 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: 'Type: Bug'
+assignees: ''
+
+---
+
+### Steps to reproduce
+1. *Step 1*
+2. *Step 2*
+3. *Step 3*
+
+### Minimal reproduction
+
+Please provide a reference to a repository that includes a minimal reproduction of the issue.
+
+We recommend spawning a new application using the [Royal Navy CRA Template](https://www.npmjs.com/package/cra-template-royalnavy):
+
+```bash
+// npx
+npx create-react-app my-app --template royalnavy
+
+// yarn create
+yarn create react-app my-app --template royalnavy
+```
+
+### Screenshot
+*Add a screenshot*
+
---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { fetchEntries } from '../services/contentful'
+import { Layout } from '../components/Layout'
+import { Post } from '../components/Post'
+import { PostType } from '../interfaces'
+
+export default function Home({ posts }) {
+  return (
+    <Layout title="NextJS + Contentful Boilerplate">
+      <main>
+        {posts.map(({ title }: PostType) => {
+          return <Post title={title} />
+        })}
+      </main>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetchEntries()
+
+  const posts = await res.map((post: any) => {
+    return post.fields
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
