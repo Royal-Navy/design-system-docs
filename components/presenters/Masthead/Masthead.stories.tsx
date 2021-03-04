9853e9cb67f68dc102ae0d498c99dc0073882fd3
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-03-04 09:37:46 +0000

    refactor(Masthead): Use callback for handling toggle

--- components/presenters/Masthead/Masthead.stories.tsx
+++ components/presenters/Masthead/Masthead.stories.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 import { Masthead } from '.'
 
 const stories = storiesOf('Masthead', module)
 
 stories.add('Default', () => (
-  <Masthead title="Royal Navy Design System" version="2.36.3" />
+  <Masthead
+    title="Royal Navy Design System"
+    version="2.36.3"
+    onToggle={action('onToggle')}
+  />
 ))
--- components/presenters/Masthead/Masthead.test.tsx
+++ components/presenters/Masthead/Masthead.test.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent } from '@testing-library/react'
 
 import { Masthead } from '.'
 
 describe('Masthead', () => {
   let wrapper: RenderResult
+  let onToggleSpy: (e: React.MouseEvent<HTMLButtonElement>) => void
 
   describe('with required props', () => {
     beforeEach(() => {
-      wrapper = render(<Masthead title="Hello, World!" version="0.1.0" />)
+      onToggleSpy = jest.fn()
+
+      wrapper = render(
+        <Masthead
+          title="Hello, World!"
+          version="0.1.0"
+          onToggle={onToggleSpy}
+        />
+      )
     })
 
     it('renders the title', () => {
@@ -20,6 +29,16 @@ describe('Masthead', () => {
 
     it('renders the version', () => {
       expect(wrapper.getByTestId('masthead-version')).toHaveTextContent('0.1.0')
+    })
+
+    describe('when the user clicks the toggle menu button', () => {
+      beforeEach(() => {
+        fireEvent.click(wrapper.getByTestId('masthead-toggle-button'))
+      })
+
+      it('invokes the supplied callback', () => {
+        expect(onToggleSpy).toHaveBeenCalledTimes(1)
+      })
     })
   })
 })
