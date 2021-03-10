8d51f0e0a1761a78355308d72121c5c36ebb7d93
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-03-10 14:31:31 +0000

    build(Babel): Transpile some additional language features

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-typescript'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
   env: {
     test: {
       plugins: [
