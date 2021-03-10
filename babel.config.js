093424a1bf06f91cd1600dc6b54faada5ee9cfee
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-03-10 10:24:47 +0000

    build(StyledComponents): Resolve build issues

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,17 @@ module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-typescript'],
   env: {
     test: {
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        [
+          'styled-components',
+          {
+            ssr: true,
+            displayName: true,
+            preprocess: false,
+          },
+        ],
+      ],
     },
   },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { createGlobalStyle } from 'styled-components'
 import '../global.scss'
 
+const GlobalStyle = createGlobalStyle``
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
