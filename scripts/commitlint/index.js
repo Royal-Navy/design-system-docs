b7782874828d277106031cd105b66384046018a2
elglup <61734183+elglup@users.noreply.github.com>
2021-03-22 16:57:39 +0000

    ci(Scripts): Format with prettier

--- scripts/commitlint/index.js
+++ scripts/commitlint/index.js
@@ -1,4 +1,4 @@
-const { Octokit } = require('../check-fixup/node_modules/@octokit/rest')
+const { Octokit } = require('@octokit/rest')
 const { default: lint } = require('@commitlint/lint')
 
 const { rules } = require('../../commitlint.config')
