bcb2331596ef150b977d459c8fc8b42efd4d52c5
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-03-25 14:42:48 +0000

    fix(Sidebar): Style APITable content sections differently

--- components/presenters/Sidebar/SidebarMenuItem.tsx
+++ components/presenters/Sidebar/SidebarMenuItem.tsx
@@ -7,15 +7,17 @@ import { StyledNavLink } from './partials/StyledNavLink'
 interface SidebarMenuItemProps extends ComponentWithClass {
   href: string
   title: string
+  isCode?: boolean
 }
 
 export const SidebarMenuItem: React.FC<SidebarMenuItemProps> = ({
   href,
   title,
+  isCode,
 }) => {
   return (
     <StyledSidebarMenuItem>
-      <StyledNavLink href={href} data-testid="sidebar-link">
+      <StyledNavLink $isCode={isCode} href={href} data-testid="sidebar-link">
         {title}
       </StyledNavLink>
     </StyledSidebarMenuItem>
