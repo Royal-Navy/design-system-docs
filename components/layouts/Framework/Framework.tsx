eb338d760fe9c7945b43de6c8cdbb1ee12fda76d
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-04-29 15:52:51 +0100

    feat(LayoutFramework): Fetch Design System version from GitHub

--- components/layouts/Framework/Framework.tsx
+++ components/layouts/Framework/Framework.tsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+/* eslint-disable camelcase */
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 
 import { Masthead } from '../../presenters/Masthead'
 import { Sidebar } from '../../presenters/Sidebar'
 import { StyledMain } from './partials/StyledMain'
 
-// eslint-disable-next-line import/extensions
-import { version } from '../../../package.json'
-
 interface LayoutFrameworkProps {
   children: React.ReactNode
   title?: string
   navigation?: React.ReactNode | React.ReactNode[]
+}
+
+async function getDesignSystemVersion(): Promise<string | null> {
+  try {
+    const res = await fetch(
+      'https://api.github.com/repos/Royal-Navy/design-system/releases/latest'
+    )
+
+    const { tag_name } = await res.json()
+
+    return tag_name
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn('Failed to fetch the Design System version!', e)
+  }
+
+  return null
 }
 
 export const LayoutFramework: React.FC<LayoutFrameworkProps> = ({
@@ -20,6 +35,13 @@ export const LayoutFramework: React.FC<LayoutFrameworkProps> = ({
   navigation,
 }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [version, setVersion] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      setVersion(await getDesignSystemVersion())
+    })()
+  }, [])
 
   return (
     <>
@@ -30,7 +52,7 @@ export const LayoutFramework: React.FC<LayoutFrameworkProps> = ({
       </Head>
       <Masthead
         title="Royal Navy Design System"
-        version={version}
+        version={version || '0.1.0'}
         onToggle={(_) => setIsOpen(!isOpen)}
       />
       <Sidebar
