10989e4ccced0e9629e3edfa97b435c273719f02
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-07-15 14:09:07 +0100

    feat(Helpers): Add to warn for overriding props

---
+++ components/helpers.ts
@@ -0,0 +1,13 @@
+function warnIfOverwriting<P>(
+  props: P,
+  propertyName: string,
+  componentName: string
+) {
+  if (props[propertyName]) {
+    console.warn(
+      `Prop \`${propertyName}\` on \`${componentName}\` will be overwritten`
+    )
+  }
+}
+
+export { warnIfOverwriting }
