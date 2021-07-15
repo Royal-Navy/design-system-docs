38aa39f6ef418cd8f63dd143e83c912570bfef06
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-07-15 10:41:40 +0100

    build(SVG): Add support for inline SVG

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // other configs...
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+            titleProp: true,
+          },
+        },
+      ],
+    })
+    return config
+  },
+}
