0955b6e20e693c9c2bf0df4d2eeb29ccc1e30bf7
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-08-09 09:29:42 +0100

    feat(CodeHighlighter): Create base presentational component

---
+++ components/presenters/Docs/CodeHighlighter/partials/StyledCodeHighlighter.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const StyledCodeHighlighter = styled.article`
+  display: flex;
+  flex-direction: column;
+  color: #273b5b;
+  overflow: hidden;
+`
