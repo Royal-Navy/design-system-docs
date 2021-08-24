893077dbfbcfb805d67608f4e8a1787f5a1b7557
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-08-24 14:57:42 +0100

    feat(HeroCard): Create presentational component

---
+++ components/presenters/Docs/HeroCard/partials/StyledTitle.tsx
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components'
+import { selectors } from '@royalnavy/design-tokens'
+import hexRgb from 'hex-rgb'
+
+const { fontSize, color } = selectors
+
+function getColorWithOpacity(hexColor: string) {
+  const values = hexRgb(hexColor, { format: 'object' })
+
+  return css`
+    rgba(${values.red}, ${values.green}, ${values.blue}, 0.23);
+  `
+}
+
+export const StyledTitle = styled.span`
+  font-size: ${fontSize('xxl')};
+  font-weight: 700;
+  text-shadow: 0px 1px 2px ${getColorWithOpacity(color('neutral', 'black'))};
+`
