bfc9a875c3489d4bdba59a19a57f04d5a94ce214
hxltrhuxze <48090803+hxltrhuxze@users.noreply.github.com>
2021-09-01 12:54:41 +0100

    feat(OnThisPage): Change position and padding

--- components/presenters/Docs/Sidebar/partials/StyledSidebar.tsx
+++ components/presenters/Docs/Sidebar/partials/StyledSidebar.tsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components'
 import { selectors } from '@royalnavy/design-tokens'
 
-const { color, shadow, spacing } = selectors
+const { color, shadow, spacing, mq } = selectors
 
 export const StyledSidebar = styled.aside`
-  width: 316px;
+  width: 248px;
   background-color: ${color('neutral', 'white')};
   border: 1px solid ${color('neutral', '100')};
   border-radius: 8px;
   box-shadow: ${shadow('2')};
   padding: ${spacing('6')};
+  ${mq({ gte: 'xl' })`
+    width: 316px;
+  `}
 `
