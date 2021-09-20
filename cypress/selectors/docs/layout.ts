859caf8a2b0a5023ab2982e04d210eba3ded4318
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-09-20 09:52:03 +0100

    fix(Masthead): Disable close on document click expanded (mobile)

--- cypress/selectors/docs/layout.ts
+++ cypress/selectors/docs/layout.ts
@@ -1,5 +1,7 @@
 export default {
   h1: '[data-testid="layout-legacy-h1"]',
   contentBanner: '[data-testid="content-banner"]',
-  breadcrumbItem: '[data-testid="breadcrumb-item"]'
+  breadcrumbItem: '[data-testid="breadcrumb-item"]',
+  mastheadToggleButton: '[data-testid="masthead-toggle-button"]',
+  mastheadMenuExpandButton: '[data-testid="masthead-menu-expand-button"]'
 }
