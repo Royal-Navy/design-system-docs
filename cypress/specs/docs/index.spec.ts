40cc849684d78d7c41ec5f443526bb2c129daa41
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-09-20 09:52:03 +0100

    fix(Masthead): Disable close on document click expanded (mobile)

--- cypress/specs/docs/index.spec.ts
+++ cypress/specs/docs/index.spec.ts
@@ -97,3 +97,54 @@ describe('Docs Site: Components', () => {
     })
   })
 })
+
+describe('Docs Site: Homepage', () => {
+  describe(
+    'when browsing on mobile',
+    {
+      viewportHeight: 1000,
+      viewportWidth: 500,
+    },
+    () => {
+      before(() => {
+        // Block newrelic.js due to issues with Cypress networking
+        cy.intercept('**/newrelic.js', (req) => {
+          req.reply("console.log('Fake New Relic script loaded');")
+        })
+
+        cy.visit('/')
+      })
+
+      describe('when the mobile navigation is opened', () => {
+        it('should open the mobile navigation', () => {
+          cy.get(selectors.layout.mastheadToggleButton).click()
+          cy.get(selectors.layout.mastheadMenuExpandButton).should('be.visible')
+        })
+
+        describe('and the first sub menu group is expanded', () => {
+          it('should expand the relevant sub menu', () => {
+            cy.get(selectors.layout.mastheadMenuExpandButton).eq(0).click()
+            cy.get('a').contains('Get started').should('be.visible')
+            cy.get('a').contains('Tokens').should('not.exist')
+          })
+
+          describe('and the second sub menu group is expanded', () => {
+            it('should expand the relevant sub menu', () => {
+              cy.get(selectors.layout.mastheadMenuExpandButton).eq(1).click()
+              cy.get('a').contains('Get started').should('be.visible')
+              cy.get('a').contains('Tokens').should('be.visible')
+            })
+
+            describe('and the first sub menu group is collapsed', () => {
+              it('should collapse the relevant sub menu', () => {
+                cy.get(selectors.layout.mastheadMenuExpandButton).eq(0).click()
+                cy.get('a').contains('Get started').should('not.exist')
+                cy.get('a').contains('Tokens').should('be.visible')
+              })
+            })
+          })
+        })
+      })
+    }
+  )
+})
