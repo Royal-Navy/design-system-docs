01dd7e4e484da3f1051db5f74126c7f99259ab5e
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-09-22 16:08:29 +0100

    fix(Footer): Update `href` of links

--- cypress/selectors/docs/index.ts
+++ cypress/selectors/docs/index.ts
@@ -1,11 +1,13 @@
 import contentBlock from './contentBlock'
 import codeBlock from './codeBlock'
+import footer from './footer'
 import layout from './layout'
 import onThisPage from './onThisPage'
 
 export default {
   contentBlock,
   codeBlock,
+  footer,
   layout,
-  onThisPage
+  onThisPage,
 }
