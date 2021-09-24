cd4cd0414d3d6cf9c3feb4aa277a26b5ff2d79bd
hxltrhuxze <48090803+hxltrhuxze@users.noreply.github.com>
2021-09-24 15:43:37 +0100

    fix(StyledCodeBlock): Make code block take up horizontal space

--- components/presenters/Framework/CodeBlock/partials/StyledCodeBlock.tsx
+++ components/presenters/Framework/CodeBlock/partials/StyledCodeBlock.tsx
@@ -5,27 +5,7 @@ const { color, spacing, mq } = selectors
 
 export const StyledCodeBlock = styled.div`
   background-color: ${color('neutral', '700')};
-  padding-top: ${spacing('8')};
   width: 100%;
-
-  ${mq({ gte: 'xl' })`
-    border-top: 4px solid ${color('neutral', '600')};
-    width: 50%;
-    padding-top: ${spacing('15')};
-    padding-bottom: ${spacing('13')};
-  `}
-
-  @media only screen and (min-width: 1900px) {
-    width: calc(100% - 900px);
-  }
-
-  &:empty {
-    display: none;
-
-    ${mq({ gte: 'xl' })`
-      display: block;
-    `}
-  }
 
   pre,
   pre[class*='language-'] {
